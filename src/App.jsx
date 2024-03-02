220d4ce809903abb3ab1e3e09d4e115aef787e45
Amrita Gupta <amamrita0818@gmail.com>
2024-03-03 00:09:30 +0530

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+        <Outlet />
+        
+    </>
   )
 }
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+import App from './App'
+import HomePage from './components/HomePage/HomePage';
+import Services from './components/Services/Services';
+import Works from './components/Works/Works';
+import Contact from './components/Contact/Contact';
+
+
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<Works />} />
+        <Route path='/products' element={<Services/>} />
+        <Route path='/contact' element={<Contact />} />
+      </Route>
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Root />
 )
