15801d3dc4fa6a59aaebf88ad5f323c029eb23c0
Amrita Gupta <amamrita0818@gmail.com>
2024-03-20 11:47:03 +0530

    update styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full bg-transparent relative ">
-      <div className="md:w-[90%] absolute top-[-1.5rem] z-50 w-full flex md:flex-row flex-col md:pl-20 md:pr-5 bg-transparent md:gap-y-0 gap-y-3  h-full max-w-[1200px]  md:justify-between justify-center items-center pb-16">
+      <div className="md:w-[90%] absolute top-[-4.5rem] z-50 w-full flex md:flex-row flex-col md:pl-20 md:pr-5 bg-transparent md:gap-y-0 gap-y-3  h-full max-w-[1200px]  md:justify-between justify-center items-center pb-16">
         <div className="flex md:flex-row flex-col gap-x-8 gap-y-3 items-center">
           <h1 className="text-black  text-[10px] font-[800] tracking-wider">SOCIAL:</h1>
           <ul className="text-black md:pl-3 text-[10px] font-[800] flex flex-row gap-x-4 tracking-wider">
